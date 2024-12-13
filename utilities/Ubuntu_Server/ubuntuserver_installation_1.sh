b9fa4581657527021b6eda2fe30269db72b8a4d3
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2024-12-13 09:57:14 -0300

    Enviando tudo.

--- utilities/Ubuntu_Server/ubuntuserver_installation_1.sh
+++ utilities/Ubuntu_Server/ubuntuserver_installation_1.sh
@@ -23,9 +23,14 @@ sleep 1
 
 blue "\nInstalando dependências necessárias...\n"
 
-sudo nala install build-essential git neofetch curl wget net-tools btop python3-venv python3-pip python3.10-full python3.10-dev dkms perl gcc make default-libmysqlclient-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev llvm xz-utils tk-dev libffi-dev liblzma-dev python3-openssl bat ruby-full nodejs npm fzf -y
+sudo nala install build-essential unzip git neofetch curl wget net-tools btop python3-venv python3-pip python3.10-full python3.10-dev dkms perl gcc make default-libmysqlclient-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev llvm xz-utils tk-dev libffi-dev liblzma-dev python3-openssl bat ruby-full nodejs npm fzf -y
 
 sleep 1
+
+sudo timedatectl set-timezone America/Sao_Paulo
+mkdir -p ~/Downloads
+mkdir -p ~/scripts
+mkdir -p ~/repos
 
 blue "\nAlterando o shell padrão bash para o zsh."
 blue "Certifique-se de digitar corretamente a senha."
